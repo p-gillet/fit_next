c19ee37ec063867c2df89c31a310babb0807a0b1
leandro <leandro262009@gmail.com>
2022-12-23 09:49:03 +0100

    simple DB SELECT example

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:postgres/postgres.dart';
 
 void main() {
   runApp(const MyApp());
@@ -28,12 +29,60 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
+class CRUDTable extends StatelessWidget {
+  const CRUDTable({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("test"),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            // Navigate back to first route when tapped.
+          },
+          child: const Text('Go back!'),
+        ),
+      ),
+    );
+  }
+}
+
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
   void _incrementCounter() {
     setState(() {
       _counter++;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    var connection = PostgreSQLConnection("localhost", 5432, "bdr",
+        username: "bdr", password: "bdr");
+
+    connection.open().then((value) {
+      connection.mappedResultsQuery("SELECT * FROM fitnext.coach;").then((res) {
+        var columnPrinted = false;
+        for (var element in res) {
+          if (!columnPrinted) {
+            columnPrinted = true;
+            for (var a in element.keys) {
+              print(a);
+            }
+          }
+
+          // print datta
+          for (var val in element.values) {
+            print(val);
+          }
+        }
+      });
     });
   }
 
@@ -74,15 +123,15 @@ class _MyHomePageState extends State<MyHomePage> {
               decoration: BoxDecoration(
                 color: Colors.blue,
               ),
-              child: Text('Drawer Header'),
+              child: Text('Liste des tables dans la base de données'),
             ),
             ListTile(
               title: const Text('Item 1'),
               onTap: () {
-                // Update the state of the app
-                // ...
-                // Then close the drawer
-                Navigator.pop(context);
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const CRUDTable()),
+                );
               },
             ),
             ListTile(
