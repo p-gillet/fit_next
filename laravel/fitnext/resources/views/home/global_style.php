fd9d7fb262ca20c955dfb41c400877cd39a6902b
Leandro <leandro262009@gmail.com>
2023-01-21 09:57:42 +0100

    Added home page

---
+++ laravel/fitnext/resources/views/home/global_style.php
@@ -0,0 +1,11 @@
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,500;1,500;1,700&display=swap" rel="stylesheet">
+<style>
+   * {
+      font-family: 'Roboto', sans-serif;
+      font-size: 25px;
+      color: #111;
+      background-color: #eee;
+   }
+</style>
--- laravel/fitnext/resources/views/home/index.php
+++ laravel/fitnext/resources/views/home/index.php
@@ -1,7 +1,53 @@
 <html>
-  <?php foreach ($models as $model): ?>
-   <div class="col-md-4">
-      <a class="text-center mt-3" href="/<?= $model?>"><?= $model ?></a>
+   <div class="container">
+      <div class="header">
+         <h1>Afficher une table</h1>
+         <div class="tables">
+            <?php foreach ($models as $model): ?>
+               <a href="/<?= $model?>"><?= $model ?></a>
+            <?php endforeach; ?>
+         </div>
+      </div>
+
+      <div class="header">
+         <h1>Afficher une vue</h1>
+         <div class="tables">
+            <p>a</p>
+            <p>b</p>
+            <p>c</p>
+         </div>
+      </div>
    </div>
-   <?php endforeach; ?>
+
+   <?php require('global_style.php'); ?>
+   
+   <style>
+      .container {
+         display: flex;
+         margin-left: 50px;
+      }
+
+      .header {
+         flex-grow: 1;
+      }
+      
+      .tables {
+         display: flex;
+         flex-direction: column;
+      }
+
+      a {
+         text-decoration: none;
+         transition: 0.2s;
+         padding: 5px;
+         margin: 5px 50px 5px 50px;
+         border-radius: 5px;
+         background-color: white;
+      }
+
+      a:hover {
+         color: white;
+         background-color: coral;
+      }
+   </style>
 </html>
